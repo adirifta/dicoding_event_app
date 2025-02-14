46ac2b44745c783952c7661f8be606a57698d00a
adirifta <adiriftadk@gmail.com>
2025-02-14 18:23:02 +0700

    Done

--- app/src/main/java/com/adicoding/dicodingeventapp/ui/detail/DetailEventActivity.kt
+++ app/src/main/java/com/adicoding/dicodingeventapp/ui/detail/DetailEventActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.text.Html
-import android.text.method.LinkMovementMethod
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
